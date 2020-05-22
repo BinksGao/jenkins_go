20382a6fa27134ce6fd8b1fe71f24b64720bd57f
BinksGao <18839127565@163.com>
2020-05-22 16:32:56 +0800

    fix port

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, Gin")
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":8090") // listen and serve on 0.0.0.0:8080
 }
