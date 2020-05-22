56f6b15e02247ee6d0d076cac6a93c6927dba8bd
BinksGao <18839127565@163.com>
2020-05-22 16:35:20 +0800

    fix port

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, Gin")
+		c.String(200, "Get请求成功!")
 	})
 	r.Run(":8090") // listen and serve on 0.0.0.0:8080
 }
